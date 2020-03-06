8038327af939822108483dce3cad0f514dc2f4dc
biley02 <biley1234roy@gmail.com>
2020-03-06 22:46:13 +0530

    add app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+var express=require("express");
+var app=express();
+var mongoose=require("mongoose");
+var bodyParser=require("body-parser");
+
+
+//configure mongoose
+mongoose.connect("mongodb://localhost/our_webpage");
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+var docSchema=new mongoose.Schema({
+    subject_code: String,
+    semester: String,
+    pdf: String,
+    body: String
+
+});
+
+app.get("/",function(req,res){
+    res.render("home.ejs");
+});
+
+app.listen(3000,function(){
+    console.log("SERVER STARTED!!");
+});
